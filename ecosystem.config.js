ab99d2bad12b1476a30235d7963c2d3f28d108ca
suhyun <chohoki@gmail.com>
2022-06-22 21:47:46 +0900

    ci: test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "app",
-      script: "./.next/standalone/server.js",
+      script: "./server.js",
       env: {
         NODE_ENV: "development",
       },
