b87a1675c1e43584d3c29b4966f48d7aec96038d
suhyun <chohoki@gmail.com>
2022-06-22 21:17:44 +0900

    ci: test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { BsAlarm } from "react-icons/bs";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -14,7 +15,11 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome tofff <a href="https://nextjs.org">Next.js!</a>
+          Welcome to{" "}
+          <a href="https://nextjs.org">
+            Next.js!
+            <BsAlarm />
+          </a>
         </h1>
 
         <p className={styles.description}>
